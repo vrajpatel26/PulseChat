f9752a39af7253100baccd5788226990965b7406
vrajpatel26 <vraj.26112004@gmail.com>
2026-05-23 15:20:34 +0530

    push

--- frontend/src/components/MessageArea.jsx
+++ frontend/src/components/MessageArea.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoArrowBack } from "react-icons/io5";
+import { GrEmoji } from "react-icons/gr";
+import { IoImages } from "react-icons/io5";
+import { IoSendSharp } from "react-icons/io5";
 import dp from "../assets/dp.png"
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedUser } from '../redux/userSlice';
@@ -7,6 +10,8 @@ import { setSelectedUser } from '../redux/userSlice';
 const MessageArea = () => {
     let { selectedUser } = useSelector(state => state.user)
     let dispatch = useDispatch()
+    let [showPicker , setShowPicker] = useState(false)
+
     return (
         <div className={`lg:w-[70%] ${selectedUser ? "flex" : "hidden"} lg:block w-full h-full bg-slate-200 border-l-2 border-gray-300 relative `}>
 
@@ -31,11 +36,34 @@ const MessageArea = () => {
                 <div className='h-full w-full flex items-center justify-center'>
                     <h1 className='text-[38px] font-semibold text-[#074d65]'>Welcome to PulseChat </h1>
                 </div>}
-            <div className='flex justify-center items-center'>
-                <div className='w-full lg:w-[50%] h-[100px] fixed bottom-[20px] flex justify-center items-center bg-red-200'>
-                    <form className=' w-[95%] max-w-[60%] h-[60px] bg-yellow-200 rounded-full'></form>
+
+            {selectedUser &&
+                <div className='absolute bottom-[20px] left-0 w-full flex justify-center items-center px-[10px]'>
+
+                    <form className='w-full max-w-[900px] h-[60px] bg-slate-300 rounded-full flex items-center gap-[20px] px-[20px]'>
+
+                        <div onClick={()=>setShowPicker(prev=>!prev)}>
+                            <GrEmoji className='text-[30px] text-gray-700 cursor-pointer' />
+                        </div>
+
+                        <input
+                            type="text"
+                            className='h-full w-full border-0 outline-none text-[20px] bg-transparent'
+                            placeholder='Message'
+                        />
+
+                        <div>
+                            <IoImages className='text-[30px] cursor-pointer' />
+                        </div>
+
+                        <div>
+                            <IoSendSharp className='text-[30px] cursor-pointer' />
+                        </div>
+
+                    </form>
+
                 </div>
-            </div>
+            }
         </div>
     )
 }
