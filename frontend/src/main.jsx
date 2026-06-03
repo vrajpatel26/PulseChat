a0c516dec62a7ed0c31e17458e648d5836a43651
vrajpatel26 <vraj.26112004@gmail.com>
2026-06-03 12:15:02 +0530

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { store } from './redux/store.js'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://pulsechat-backend-2bu1.onrender.com"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
