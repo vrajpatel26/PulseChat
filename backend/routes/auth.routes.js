ed494744bc105f6240ff12a7ae88e65696e29fa8
vrajpatel26 <vraj.26112004@gmail.com>
2026-06-04 00:33:05 +0530

    update auth  route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { login, logout, signup } from "../controllers/auth.controllers.js"
+import isAuth from "../middlewares/isAuth.js"
 
 const authRouter = express.Router()
 
 authRouter.post("/signup",signup)
 authRouter.post("/login",login)
-authRouter.get("/logout",logout)
+authRouter.get("/logout",isAuth,logout)
 
 export default authRouter;
