ce4af6ebcdb4ed0b17f403d673c21304bb0c21db
vrajpatel26 <vraj.26112004@gmail.com>
2026-06-04 00:19:20 +0530

    update

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -84,9 +84,20 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.clearCookie("token")
-        return res.status(200).json({ message: "logout successfully" })
+
+        await User.findByIdAndUpdate(req.userId, {
+            lastSeen: new Date()
+        });
+
+        res.clearCookie("token");
+
+        return res.status(200).json({
+            message: "logout successfully"
+        });
+
     } catch (error) {
-        return res.status(500).json({ message: `login error ${error}` })
+        return res.status(500).json({
+            message: `logout error ${error}`
+        });
     }
 }
