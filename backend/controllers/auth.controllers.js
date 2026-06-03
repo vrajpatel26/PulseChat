d1e7cea73f6cf87140bddaaba2741b7e2e147364
vrajpatel26 <vraj.26112004@gmail.com>
2026-06-03 12:21:54 +0530

    Update auth.middleware.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -36,8 +36,8 @@ export const signup = async (req, res) => {
         res.cookie("token", token, ({
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "Strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         }))
 
         return res.status(201).json(user)
@@ -70,8 +70,8 @@ export const login = async (req, res) => {
         res.cookie("token", token, ({
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "Strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         }))
 
         return res.status(200).json(user)
